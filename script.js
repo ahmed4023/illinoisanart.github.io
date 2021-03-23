05f6c8d3bf592d802065a9220608ae87af4375fa
ahmed4023 <58666316+ahmed4023@users.noreply.github.com>
2021-03-23 19:56:34 +0800

    file upload

---
+++ script.js
@@ -0,0 +1,16 @@
+ function videoLink(play) 
+        {
+          document.getElementById("slider").src = play;
+        }
+
+
+
+
+       /*
+        </section>
+        <script type="text/javascript">
+          function videoLink(play) 
+        {
+          document.getElementById("slider").src = play;
+        }
+        </script>*/
